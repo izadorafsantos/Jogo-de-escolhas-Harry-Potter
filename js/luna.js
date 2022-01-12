106e74e9e807c7290a5b493c0635d092d438e58d
Izadora Ferreira <izadorasantos20@hotmail.com>
2022-01-12 02:22:02 -0300

    atualização das paginas

--- js/luna.js
+++ js/luna.js
@@ -26,10 +26,16 @@ function fase3luna() {
     var escolhajogador = prompt(`Digite 1 ou 2`);
 
     if (escolhajogador ==1) {
-        window.location.href = "../html/gameoverluna3.html";
-    } else if (escolhajogador ==2){
         window.location.href = "../html/lunawin.html";
+    } else if (escolhajogador ==2){
+        window.location.href = "../html/gameoverluna3.html";
     } else {
         alert (`Digite 1 ou 2`);
     }
+}
+
+/* ------- GAMER OVER PARA O INÍCIO -------*/
+let lunareinicio1 = document.querySelector("#lunareinicio1").onclick = function(){
+    let lunareinicio2 = document.querySelector("#lunareinicio2");
+    lunareinicio2.setAttribute("href", "./index.html");
 }
